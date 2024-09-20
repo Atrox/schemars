25c84749bee47072a9cdac8264835a54b5e76a0d
Atrox <hello@atrox.dev>
2024-09-20 23:51:27 +0200

    add time feature

--- schemars/src/json_schema_impls/time.rs
+++ schemars/src/json_schema_impls/time.rs
@@ -1,57 +1,40 @@
+use std::borrow::Cow;
+
+use time::{Date, OffsetDateTime, PrimitiveDateTime, Time};
+
 use crate::r#gen::SchemaGenerator;
 use crate::schema::*;
-use crate::JsonSchema;
-use std::borrow::Cow;
-use std::time::{Duration, SystemTime};
+use crate::{JsonSchema, Schema};
 
-impl JsonSchema for Duration {
-    fn schema_name() -> String {
-        "Duration".to_owned()
-    }
+macro_rules! formatted_string_impl {
+    ($ty:ident, $format:literal) => {
+        formatted_string_impl!($ty, $format, JsonSchema for $ty);
+    };
+    ($ty:ident, $format:literal, $($desc:tt)+) => {
+        impl $($desc)+ {
+            no_ref_schema!();
 
-    fn schema_id() -> Cow<'static, str> {
-        Cow::Borrowed("std::time::Duration")
-    }
+            fn schema_name() -> String {
+                stringify!($ty).to_owned()
+            }
 
-    fn json_schema(generator: &mut SchemaGenerator) -> Schema {
-        let mut schema = SchemaObject {
-            instance_type: Some(InstanceType::Object.into()),
-            ..Default::default()
-        };
-        let obj = schema.object();
-        obj.required.insert("secs".to_owned());
-        obj.required.insert("nanos".to_owned());
-        obj.properties
-            .insert("secs".to_owned(), <u64>::json_schema(generator));
-        obj.properties
-            .insert("nanos".to_owned(), <u32>::json_schema(generator));
-        schema.into()
-    }
+            fn schema_id() -> Cow<'static, str>  {
+                Cow::Borrowed(stringify!(time::$ty))
+            }
+
+            fn json_schema(_: &mut SchemaGenerator) -> Schema {
+                SchemaObject {
+                    instance_type: Some(InstanceType::String.into()),
+                    format: Some($format.to_owned()),
+                    ..Default::default()
+                }
+                .into()
+            }
+        }
+    };
 }
 
-impl JsonSchema for SystemTime {
-    fn schema_name() -> String {
-        "SystemTime".to_owned()
-    }
-
-    fn schema_id() -> Cow<'static, str> {
-        Cow::Borrowed("std::time::SystemTime")
-    }
-
-    fn json_schema(generator: &mut SchemaGenerator) -> Schema {
-        let mut schema = SchemaObject {
-            instance_type: Some(InstanceType::Object.into()),
-            ..Default::default()
-        };
-        let obj = schema.object();
-        obj.required.insert("secs_since_epoch".to_owned());
-        obj.required.insert("nanos_since_epoch".to_owned());
-        obj.properties
-            .insert("secs_since_epoch".to_owned(), <u64>::json_schema(generator));
-        obj.properties.insert(
-            "nanos_since_epoch".to_owned(),
-            <u32>::json_schema(generator),
-        );
-        schema.into()
-    }
-}
+formatted_string_impl!(Date, "date");
+formatted_string_impl!(PrimitiveDateTime, "partial-date-time");
+formatted_string_impl!(Time, "time");
+formatted_string_impl!(OffsetDateTime, "date-time");
